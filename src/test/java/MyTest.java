3c98a6cef9ec3dc32b431d93ca1a7ad56363b701
chen4393c <chen4393c@163.com>
2020-02-16 17:47:20 +0800

    basic calculator

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,8 @@
+import com.mashibing.inter.MyCalculator;
+
+public class MyTest {
+    public static void main(String[] args) {
+        MyCalculator myCalculator = new MyCalculator();
+        System.out.println(myCalculator.add(1, 2));
+    }
+}
