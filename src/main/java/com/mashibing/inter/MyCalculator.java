92eb6ebfa858c23a17df19b906dcf9cb71980a80
chen4393c <chen4393c@163.com>
2020-02-16 17:56:33 +0800

    logging

--- src/main/java/com/mashibing/inter/MyCalculator.java
+++ src/main/java/com/mashibing/inter/MyCalculator.java
@@ -2,22 +2,30 @@ package com.mashibing.inter;
 
 public class MyCalculator implements Calculator {
     public int add(int i, int j) {
+        System.out.println("add 方法开始执行，参数为：" + i + ", " + j);
         int result = i + j;
+        System.out.println("add 方法结束执行，结果为：" + result);
         return result;
     }
 
     public int sub(int i, int j) {
+        System.out.println("sub 方法开始执行，参数为：" + i + ", " + j);
         int result = i - j;
+        System.out.println("sub 方法结束执行，结果为：" + result);
         return result;
     }
 
     public int mul(int i, int j) {
+        System.out.println("mul 方法开始执行，参数为：" + i + ", " + j);
         int result = i * j;
+        System.out.println("mul 方法结束执行，结果为：" + result);
         return result;
     }
 
     public int div(int i, int j) {
+        System.out.println("div 方法开始执行，参数为：" + i + ", " + j);
         int result = i / j;
+        System.out.println("div 方法结束执行，结果为：" + result);
         return result;
     }
 }
--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -3,6 +3,6 @@ import com.mashibing.inter.MyCalculator;
 public class MyTest {
     public static void main(String[] args) {
         MyCalculator myCalculator = new MyCalculator();
-        System.out.println(myCalculator.add(1, 2));
+        myCalculator.add(1, 2);
     }
 }
